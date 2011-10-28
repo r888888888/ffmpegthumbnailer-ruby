6b2b047828472db88b29e0c0f95e58f2062d970f
albert <r888888888@gmail.com>
2011-10-28 15:01:22 -0400

    removed debug statemnets

--- test/ffmpegthumbnailer_test.rb
+++ test/ffmpegthumbnailer_test.rb
@@ -5,7 +5,6 @@ require 'fileutils'
 class FfmpegthumbnailerTest < Test::Unit::TestCase
   def test_resize
     path = File.join(File.dirname(File.expand_path(".", __FILE__)), "flash.swf")
-    puts path
     Ffmpegthumbnailer.resize(path, "/tmp/flash.jpg", 150)
     assert(File.size("/tmp/flash.jpg") > 0, "Thumbnail should have been created")
     FileUtils.rm_f("/tmp/flash.jpg")
